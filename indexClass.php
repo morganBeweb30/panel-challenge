3a99e4a491e42b67ebd66dba525a51987a9fca26
morganBeweb30 <morg.leen@ouvaton.org>
2020-04-08 11:30:20 +0200

    optimisation code

--- indexClass.php
+++ indexClass.php
@@ -8,27 +8,16 @@ echo "<br>";
 
 class Panel {
     private $fichier;
-    private $nb;
-//    private $tir;
-//    private $ress;
-//    private $couleur;
-    private $couleurs;
-    private $nombres;
-    private $votes;
-    private $meilleursVotes;
 
     /************   GETTERS *************/
 
     public function get_fichier() {
         return $this->fichier = file('newfile.txt');
     }
-/*    public function get_total() {
-        return $this->total = count($this->fichier);
-    }   */
-    public function get_nb() {
+/*    public function get_nb() {
 //        return $this->nb = rand(3, 10);
         return $this->nb = $this->fichier[0];
-    }
+    }   */
 /*    public function get_tir() {
         return $this->tir = 0;
     }   */
@@ -39,24 +28,6 @@ class Panel {
         shuffle($this->ress);
         return $this->couleur = $this->ress[0];
     }   */
-/*    public function get_couleurs() {
-        return $this->couleurs;
-    }   */
-/*    public function get_votes() {
-        return $this->votes = ['rouge'=>0, 'bleu'=>0, 'jaune'=>0, 'orange'=>0, 'vert'=>0, 'blanc'=>0];
-    }   */
-    public function get_couleurs() {
-        return $this->couleurs = array();
-    }
-    public function get_nombres() {
-        return $this->nombres = array();
-    }
-    public function get_votes() {
-        return $this->votes = array();
-    }
-    public function get_meilleursVotes() {
-        return $this->meilleursVotes;
-    }
 
     /************   SETTERS *************/
 
@@ -67,19 +38,22 @@ class Panel {
     /***********    FCTS SPECIFIQUES ****/
 
     public function panel_votes() {
-        echo 'Nombre de répondants : '.$this->nb.'<br>';
-        echo 'Votes : <br>';
         $total = count($this->fichier);
+        $nb = $this->fichier[0];
+        $couleurs = array();
+        $nombres = array();
+        echo 'Nombre de répondants : '.$nb=$this->fichier[0].'<br>';
+        echo 'Votes : <br>';
+        
         for($i=1;$i<$total;$i++) {
             $couleur = trim($this->fichier[$i]);
             echo $couleur.'<br>';
-        //    $couleur = $fichier[$i];
-            if(in_array($couleur, $this->couleurs)) {
-                $index = array_search($couleur, $this->couleurs);
-                $this->nombres[$index] += 1;
+            if(in_array($couleur, $couleurs)) {
+                $index = array_search($couleur, $couleurs);
+                $nombres[$index] += 1;
             } else {
-                array_push($this->couleurs, $couleur);
-                array_push($this->nombres, 1);
+                array_push($couleurs, $couleur);
+                array_push($nombres, 1);
             }
         }
 /*        while($this->tir<$this->nb) {
@@ -91,35 +65,25 @@ class Panel {
 //            $this->nb=10;
         }   */
         echo '<br>';
-        $votes = array_combine($this->couleurs, $this->nombres);
+        $votes = array_combine($couleurs, $nombres);
         asort($votes);
         foreach($votes as $coul => $value) {
             echo $coul.' : '.$value.'<br>';
         }
-        $this->meilleursVotes = array_key_last($votes);
-//        echo $this->meilleursVotes;
+        $meilleursVotes = array_key_last($votes);
         array_pop($votes);
-        $this->meilleursVotes = $this->meilleursVotes.' '.array_key_last($votes);
-        echo '<br>Meilleurs votes : <strong>'.$this->meilleursVotes.'</strong><br>'; 
+        $meilleursVotes = $meilleursVotes.' '.array_key_last($votes);
+        echo '<br>Meilleurs votes : <strong>'.$meilleursVotes.'</strong><br>'; 
     }
 }
 
 $panel = new Panel();
 $panel->get_fichier();
-echo $panel->get_nb();
-echo '<br>';
-$panel->get_couleurs();
-$panel->get_nombres();
-echo '<br>';
-$panel->get_votes();
-$panel->get_meilleursVotes();
 echo '<br>';
 $panel->panel_votes();
 echo '<br>';
 
 /*
-$tir = 0;
-//  tableau de choix des couleurs
 
 /******************* code de BD *******************************
 
